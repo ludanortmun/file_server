0888ef84b85166de6c96c7e1496112946208e542
Erick Durán <erick@Ericks-MacBook-Pro.local>
2018-05-03 18:19:36 -0700

    send message

--- client.py
+++ client.py
@@ -6,5 +6,15 @@ address = '10.12.41.114'
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server_address = (address, 3000)
-print >> sys.stderr, 'connecting to %s port %s' % server_address
+print ('connecting to %s port %s' % server_address)
 sock.connect(server_address)
+
+
+message = 'Glass'
+coded = message.encode('ascii')
+    
+sock.send(coded)
+
+sock.close()
+print ('closing socket')
+    
